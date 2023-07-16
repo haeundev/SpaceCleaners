511df23c089beb440e7e58c6f2b2cf1ec3e56d04
haeundev <haeundev@gmail.com>
2023-07-16 18:06:29 +0900

    grapple rnd

---
+++ Assets/DevFeatures/Grapple/GrappleEffect.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class GrappleEffect : MonoBehaviour
+{
+    [Tooltip("The speed of the entire effect.")]
+    public float Speed = 3f;
+
+    [Tooltip("The speed of the spiral offset (relative to 'Speed').")]
+    public float SpiralSpeed = 4f;
+
+    [Tooltip("The speed that the end of the line moves to the target point (relative to 'Speed')")]
+    public float DistanceSpeed = 2f;
+
+    [Tooltip("A multiplier for the pull of gravity.")]
+    public float Gravity = 0.5f;
+
+    [Tooltip("The number of points to be drawn by the line renderer. \nUpdates every time the effect is triggered.")]
+    public int Segments = 100;
+
+    [Header("Spiral")] [Tooltip("The strength of the spiral.")]
+    public Vector2 Magnitude = Vector2.one;
+
+    [Tooltip("The number of 'Curve' repetitions per world-unit.")]
+    public float Frequency = 0.5f;
+
+    [Tooltip(
+        "The amount the horizontal part of the spiral is offset along 'Curve.' \nIf 'Curve' is a sine wave, 0.25 will result in a perfect spiral.")]
+    public float HorizontalOffset = 0.25f;
+
+    [Header("Noise")] [Tooltip("The strength of the noise.")]
+    public float Strength = 0.5f;
+
+    [Tooltip("The scale of the noise samples. \nHigher = smoother.")]
+    public float Scale = 0.25f;
+
+    [Header("Curves")]
+    [Tooltip(
+        "The base curve that the points will be offset along. \nA sine wave will make it look like a grapple spiral.")]
+    public AnimationCurve Curve = new();
+
+    [Tooltip("The strength of the spiral and noise over time.")]
+    public AnimationCurve MagnitudeOverTime = new();
+
+    [Tooltip("The strength of the spiral and noise from the start to current end point within a 0 to 1 range.")]
+    public AnimationCurve MagnitudeOverDistance = new();
+
+    [Tooltip(
+        "The vertical offset applied in worldspace to the line from the start to current end point within a 0 to 1 range.")]
+    public AnimationCurve GravityOverDistance = new();
+
+    [Tooltip("The strength of the gravity over time.")]
+    public AnimationCurve GravityOverTime = new();
+
+    private float scaledTimeOffset;
+    private float spiralTimeOffset;
+    private float lastGrappleTime;
+    private Vector3 grapplePoint;
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    public void DoGrapple()
+    {
+        if (lineRenderer.enabled = Physics.Raycast(transform.position, transform.forward, out var hit))
+            DoGrapple(hit.point);
+    }
+
+    public void DoGrapple(Vector3 grapplePoint)
+    {
+        this.grapplePoint = grapplePoint;
+        scaledTimeOffset = spiralTimeOffset = 0f;
+        if (lineRenderer.positionCount != Segments)
+            lineRenderer.positionCount = Segments;
+
+        lastGrappleTime = Time.time * 10f;
+    }
+
+    // I like this naming convention haha
+    public void StopGrapple()
+    {
+        lineRenderer.enabled = false;
+    }
+
+    public void SetGrapplePoint(Vector3 grapplePoint)
+    {
+        this.grapplePoint = grapplePoint;
+    }
+
+    private void Update()
+    {
+        if (!lineRenderer.enabled)
+            return;
+
+        var difference = grapplePoint - transform.position;
+        var direction = difference.normalized;
+        var distanceMultiplier = Mathf.Clamp01(scaledTimeOffset * DistanceSpeed);
+        var distance = difference.magnitude * distanceMultiplier;
+
+        scaledTimeOffset += Speed * Time.deltaTime;
+        if (distanceMultiplier < 1f)
+            spiralTimeOffset += Speed * SpiralSpeed * Time.deltaTime;
+
+        for (var i = 0; i < lineRenderer.positionCount; i++)
+        {
+            var t = (float)i / lineRenderer.positionCount;
+            var position = transform.position;
+            var forwardOffset = direction * (t * distance);
+            position += forwardOffset;
+
+            var curveSamplePosition = forwardOffset.magnitude * Frequency - spiralTimeOffset;
+
+            var verticalOffset = transform.up * Curve.Evaluate(curveSamplePosition);
+            var horizontalOffset = transform.right * Curve.Evaluate(curveSamplePosition + HorizontalOffset);
+
+            verticalOffset *= Magnitude.y;
+            horizontalOffset *= Magnitude.x;
+
+            var noiseSamplePosition = -t * Scale + scaledTimeOffset + lastGrappleTime;
+            verticalOffset += transform.up * ((Mathf.PerlinNoise(0f, noiseSamplePosition) - 0.5f) * 2f * Strength);
+            horizontalOffset += transform.right * ((Mathf.PerlinNoise(noiseSamplePosition, 0f) - 0.5f) * 2f * Strength);
+
+            var magnitude = MagnitudeOverTime.Evaluate(scaledTimeOffset) * MagnitudeOverDistance.Evaluate(t);
+            verticalOffset *= magnitude;
+            horizontalOffset *= magnitude;
+
+            position += verticalOffset;
+            position += horizontalOffset;
+
+            position += Vector3.up *
+                        (GravityOverDistance.Evaluate(t) * GravityOverTime.Evaluate(scaledTimeOffset) * Gravity);
+
+            lineRenderer.SetPosition(i, position);
+        }
+    }
+}
---
+++ Assets/DevFeatures/Grapple/Grappler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Grappler : MonoBehaviour
+{
+    private GrappleEffect _grappleEffect;
+    private Camera _camera;
+    [SerializeField] private InputActionProperty button;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+        _grappleEffect = GetComponent<GrappleEffect>();
+    }
+
+    private void Update()
+    {
+        if (button.action.WasPressedThisFrame())
+        {
+            var ray = new Ray(transform.position, transform.forward);
+            Debug.DrawRay(ray.origin, ray.direction * 10f, Color.cyan, .1f);
+            if (Physics.Raycast(ray, out var info, 100/*, 1 << LayerMask.NameToLayer("Asteroid")*/))
+                Debug.Log($"hit {info.collider.name}");
+            if (info.collider == null)
+                return;
+            
+            var hitObj = info.collider.gameObject;
+            _grappleEffect.transform.LookAt(info.point);
+            _grappleEffect.DoGrapple();
+            
+            Debug.Log("Do Grapple");
+        }
+        else if (button.action.WasReleasedThisFrame())
+        {
+            _grappleEffect.StopGrapple();
+            Debug.Log("Stop Grapple");
+        }
+    }
+}
