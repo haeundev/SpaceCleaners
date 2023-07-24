c092fabb239ae71e19c90db085443380669d2fd6
haeundev <haeundev@gmail.com>
2023-07-25 02:36:33 +0900

    mitigate motion sickness for rotations

--- Assets/SpacecraftJoystickBehaviour.cs
+++ Assets/SpacecraftJoystickBehaviour.cs
@@ -6,7 +6,7 @@ public class SpacecraftJoystickBehaviour : MonoBehaviour
     private Transform _playerTransform;
     private SpacePlayer _spacePlayer;
     private XRJoystick _joystick;
-
+    [SerializeField] private float mitigateForSickness = 0.3f;
 
     private void Awake()
     {
@@ -18,9 +18,7 @@ public class SpacecraftJoystickBehaviour : MonoBehaviour
 
     private void Update()
     {
-        _playerTransform.Rotate(
-            Vector3.forward * (_joystick.value.x * (_spacePlayer.RotateSpeed * -1 * Time.deltaTime)));
-        _playerTransform.Rotate(-Vector3.right *
-                                (_joystick.value.y * (_spacePlayer.RotateSpeed * -1 * Time.deltaTime)));
+        _playerTransform.Rotate(Vector3.forward * (_joystick.value.x * mitigateForSickness * (_spacePlayer.RotateSpeed * -1 * Time.deltaTime)));
+        _playerTransform.Rotate(-Vector3.right * (_joystick.value.y * mitigateForSickness * (_spacePlayer.RotateSpeed * -1 * Time.deltaTime)));
     }
 }
