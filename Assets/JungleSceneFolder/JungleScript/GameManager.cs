a8f2a5202ebeedacb6bf2f1c3d1aa8cc15c614f2
jeejul1010 <julie0402@gmail.com>
2023-08-01 16:24:28 +0900

    watering - simple inventory ui - chestbox key done!

--- Assets/JungleSceneFolder/JungleScript/GameManager.cs
+++ Assets/JungleSceneFolder/JungleScript/GameManager.cs
@@ -6,10 +6,16 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public int[] ingredientCount = new int[3]; //0: leaf, 1: flower, 2: bug
+    int maxIngredientCount = 1;
+
+    public Transform keySpawnPos;
+    public GameObject keyItem;
 
     public float oxygenAmount;
 
     public TMP_Text[] ingredientCountText;
+
+    bool isKeySpawned = false;
 
     // Start is called before the first frame update
     void Awake()
@@ -20,7 +26,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isKeySpawned && ingredientCount[0] >= maxIngredientCount && ingredientCount[1] >= maxIngredientCount && ingredientCount[2] >= maxIngredientCount)
+        {
+            Instantiate(keyItem, keySpawnPos.position, keySpawnPos.rotation);
+            isKeySpawned = true;
+        }
     }
 
 }
