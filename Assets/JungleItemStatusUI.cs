908dd801cffd330b1867d5db0dfeb4b8ecda3d94
haeundev <haeundev@gmail.com>
2023-08-02 22:03:48 +0900

    jungle inventory, etc.

---
+++ Assets/JungleItemStatusUI.cs
@@ -0,0 +1,16 @@
+using GRASBOCK.XR.Inventory;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JungleItemStatusUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Image image;
+    [SerializeField] public ItemInfo itemInfo;
+
+    public void SetCount(int count)
+    {
+        text.text = $"{count}/5";
+    }
+}
---
+++ Assets/Prefabs/Jungle/PlantType.cs
@@ -0,0 +1,7 @@
+public enum PlantType
+{
+    Leaf,
+    Flower,
+    Bug,
+    Oxygen
+}
