d45e603be535b930d81a7056661f60f001c5d04f
jeejul1010 <julie0402@gmail.com>
2023-08-11 18:46:31 +0900

    wig ingredient ready!

--- Assets/CheckPlayerCollision.cs
+++ Assets/CheckPlayerCollision.cs
@@ -6,6 +6,9 @@ public class CheckPlayerCollision : MonoBehaviour
 {
     void OnControllerColliderHit(ControllerColliderHit hit) //character controller collision 여기서 처리!
     {
-        print(hit.gameObject);
+        if(hit.gameObject.tag == "Enemy")
+        {
+            print("Enemy hit!!");
+        }
     }
 }
