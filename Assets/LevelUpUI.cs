44a6ef445c520d44afc11c7cf321c3165be6d17d
haeundev <haeundev@gmail.com>
2023-08-13 23:08:11 +0900

    ui tweening

--- Assets/LevelUpUI.cs
+++ Assets/LevelUpUI.cs
@@ -1,9 +1,15 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUpUI : MonoBehaviour
 {
+
+    
     private void Awake()
     {
         TaskManager.Instance.levelUpUI = gameObject;
     }
+
+    
 }
