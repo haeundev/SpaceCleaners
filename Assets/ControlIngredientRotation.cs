0d348d2d5cdf75728a6e8352db6760deeac4a14c
jeejul1010 <julie0402@gmail.com>
2023-09-16 15:52:00 +0900

    starting on crafting zone

--- Assets/ControlIngredientRotation.cs
+++ Assets/ControlIngredientRotation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ControlIngredientRotation : MonoBehaviour
@@ -12,33 +13,42 @@ public class ControlIngredientRotation : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        int layerMask = 1 << 28;
-        
-        if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.3f, layerMask)) //transform.position
-        {
-            print("Debris on top");
-            gameObject.GetComponent<SimpleRotator>().enabled = true;
-            // if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Debris"))
-            // {
-            //     print("Debris on top");
-            //     hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = true;
-            // }
-            // else
-            // {
-            //     print("Something on top");
-            // }
-            // if (hit.point == gameObject.tag("IngredientHolder"))
-            // {
-            //     print("Above");
-            // }
-        }
-        else
-        {
-            print("nothing on top");
-            gameObject.GetComponent<SimpleRotator>().enabled = false;
-        }
+    // void Update()
+    // {
+    //     int layerMask = 1 << 28;
+    //     
+    //     if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.3f, layerMask)) //transform.position
+    //     {
+    //         print("Debris on top");
+    //         gameObject.GetComponent<SimpleRotator>().enabled = true;
+    //         // if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Debris"))
+    //         // {
+    //         //     print("Debris on top");
+    //         //     hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = true;
+    //         // }
+    //         // else
+    //         // {
+    //         //     print("Something on top");
+    //         // }
+    //         // if (hit.point == gameObject.tag("IngredientHolder"))
+    //         // {
+    //         //     print("Above");
+    //         // }
+    //     }
+    //     else
+    //     {
+    //         print("nothing on top");
+    //         gameObject.GetComponent<SimpleRotator>().enabled = false;
+    //     }
+    //
+    // }
 
+    void OnTriggerEnter(Collider other) //collider 만나면 가운데로 위치 / 회전하게
+    {
+        if (other.gameObject.tag == "IngredientHolder")
+        {
+            transform.position = other.gameObject.transform.position;
+            gameObject.GetComponent<SimpleRotator>().enabled = true;
+        }
     }
 }
--- Assets/CraftingZoneManager.cs
+++ Assets/CraftingZoneManager.cs
@@ -15,37 +15,44 @@ public class CraftingZoneManager : MonoBehaviour
 
     public Transform[] spawnIngredientsPos;
     
-    public Transform[] craftIngredientsPos;
-    
-    private RaycastHit[] hit = new RaycastHit[2];
-
-    private string[] ingredientA = {"Metal", "Plastic"};
-    private string[] ingredientB = {"Plastic", "Metal"};
-
-    private int indexA = -1;
-    private int indexB = -1;
-
-    private bool isReadyCraft = false;
-    
-    //Crafting UI
-    public GameObject craftingSliderUI;
-    private Slider craftSlider;
-    public TMP_Text craftingText;
-
-    public GameObject[] craftedObj;
-    public Transform craftedObjPos;
+    // public Transform[] craftIngredientsPos;
+    //
+    // private RaycastHit[] hit = new RaycastHit[2];
+    //
+    // private string[] ingredientA = {"Metal", "Plastic"};
+    // private string[] ingredientB = {"Plastic", "Metal"};
+    //
+    // private int indexA = -1;
+    // private int indexB = -1;
+    //
+    // private bool isReadyCraft = false;
+    //
+    // //Crafting UI
+    // public GameObject craftingSliderUI;
+    // private Slider craftSlider;
+    // public TMP_Text craftingText;
+    //
+    // public GameObject[] craftedObj;
+    // public Transform craftedObjPos;
 
     private void Awake()
     {
+        OuterSpaceEvent.OnDebrisCaptured += OnDebrisCaptured;
+        
         slider = processingSlider.GetComponent<Slider>();
-        craftSlider = craftingSliderUI.GetComponent<Slider>();
-        craftingText.text = "Place to Craft";
+        // craftSlider = craftingSliderUI.GetComponent<Slider>();
+        // craftingText.text = "Place to Craft";
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(ProcessingBar(duration));
+        StartCoroutine(ProcessingBar(duration)); //for testing
+    }
+
+    void OnDebrisCaptured(DebrisType _, GameObject __)
+    {
+        //StartCoroutine(ProcessingBar(duration)); //실제는 이거!!
     }
 
     IEnumerator ProcessingBar(float duration)
@@ -65,135 +72,66 @@ public class CraftingZoneManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Physics.Raycast(craftIngredientsPos[0].position, Vector3.up, out hit[0], 0.2f)) //transform.position
-        {
-            indexA = Array.IndexOf(ingredientA, hit[0].transform.gameObject.tag);
-            // if (indexA)
-            // {
-            //     isIngredient[0] = true;
-            //     ingredientTag[0] = hit[0].transform.gameObject.tag;
-            // }
-            // else
-            // {
-            //     isIngredient[0] = false;
-            //     ingredientTag[0] = "";
-            // }
-            
-            
-            // if (hit[0].transform.gameObject.tag == "Metal" || hit[0].transform.gameObject.tag == "Plastic")
-            // {
-            //     ingredientTag[0] = hit[0].transform.gameObject.tag;
-            // }
-            
-            //print("Debris on top");
-            // hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = true;
-            // if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Debris"))
-            // {
-            //     print("Debris on top");
-            //     hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = true;
-            // }
-            // else
-            // {
-            //     print("Something on top");
-            // }
-            // if (hit.point == gameObject.tag("IngredientHolder"))
-            // {
-            //     print("Above");
-            // }
-        }
-        else
-        {
-            indexA = -1;
-        }
-        
-        if (Physics.Raycast(craftIngredientsPos[1].position, Vector3.up, out hit[1], 0.2f)) //transform.position
-        {
-            indexB = Array.IndexOf(ingredientB, hit[1].transform.gameObject.tag);
-            // if (indexA)
-            // {
-            //     isIngredient[0] = true;
-            //     ingredientTag[0] = hit[0].transform.gameObject.tag;
-            // }
-            // else
-            // {
-            //     isIngredient[0] = false;
-            //     ingredientTag[0] = "";
-            // }
-            
-            
-            // if (hit[0].transform.gameObject.tag == "Metal" || hit[0].transform.gameObject.tag == "Plastic")
-            // {
-            //     ingredientTag[0] = hit[0].transform.gameObject.tag;
-            // }
-            
-            //print("Debris on top");
-            // hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = true;
-            // if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Debris"))
-            // {
-            //     print("Debris on top");
-            //     hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = true;
-            // }
-            // else
-            // {
-            //     print("Something on top");
-            // }
-            // if (hit.point == gameObject.tag("IngredientHolder"))
-            // {
-            //     print("Above");
-            // }
-        }
-        else
-        {
-            indexB = -1;
-        }
-        
-        if (indexA != -1 && indexA == indexB)
-        {
-            print("Able to craft!");
-            craftingText.text = "Ready to Craft";
-            isReadyCraft = true;
-        }
-        // else
-        // {
-        //     //print("nothing on top");
-        //     // hit.transform.gameObject.GetComponent<SimpleRotator>().enabled = false;
-        // }
-    }
+    // void Update()
+    // {
+    //     if (Physics.Raycast(craftIngredientsPos[0].position, Vector3.up, out hit[0], 0.2f)) //transform.position
+    //     {
+    //         indexA = Array.IndexOf(ingredientA, hit[0].transform.gameObject.tag);
+    //     }
+    //     else
+    //     {
+    //         indexA = -1;
+    //     }
+    //     
+    //     if (Physics.Raycast(craftIngredientsPos[1].position, Vector3.up, out hit[1], 0.2f)) //transform.position
+    //     {
+    //         indexB = Array.IndexOf(ingredientB, hit[1].transform.gameObject.tag);
+    //     }
+    //     else
+    //     {
+    //         indexB = -1;
+    //     }
+    //     
+    //     if (indexA != -1 && indexA == indexB)
+    //     {
+    //         print("Able to craft!");
+    //         craftingText.text = "Ready to Craft";
+    //         isReadyCraft = true;
+    //     }
+    // }
 
-    public void OnCraftButtonPressed()
-    {
-        if (isReadyCraft)
-        {
-            print("crafting~");
-            craftingText.text = "Crafting...";
-            StartCoroutine(CraftingBar(duration));
-            
-        }
-    }
-    
-    public void OnCraftButtonReleased()
-    {
-        isReadyCraft = false;
-    }
-    
-    IEnumerator CraftingBar(float duration)
-    {
-        float time = 0;
-        while (time < duration)
-        {
-            craftSlider.value = time / duration;
-            time += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-        
-        print("craft complete~");
-        craftingText.text = "Craft Complete";
-        
-        Instantiate(craftedObj[indexA], craftedObjPos.transform.position, Quaternion.identity);
-        
-    }
+    // public void OnCraftButtonPressed()
+    // {
+    //     if (isReadyCraft)
+    //     {
+    //         print("crafting~");
+    //         craftingText.text = "Crafting...";
+    //         StartCoroutine(CraftingBar(duration));
+    //         
+    //     }
+    // }
+    //
+    // public void OnCraftButtonReleased()
+    // {
+    //     isReadyCraft = false;
+    // }
+    //
+    // IEnumerator CraftingBar(float duration)
+    // {
+    //     float time = 0;
+    //     while (time < duration)
+    //     {
+    //         craftSlider.value = time / duration;
+    //         time += Time.deltaTime;
+    //         yield return new WaitForEndOfFrame();
+    //     }
+    //     
+    //     print("craft complete~");
+    //     craftingText.text = "Craft Complete";
+    //     
+    //     Instantiate(craftedObj[indexA], craftedObjPos.transform.position, Quaternion.identity);
+    //     
+    // }
     
     
 }
