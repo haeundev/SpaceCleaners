dc7477df0f905f53227af0b1c58383330885c8c2
haeundev <haeundev@gmail.com>
2023-09-16 18:37:03 +0900

    boundary warning sound

--- Assets/SpaceshipBoundaryWarning.cs
+++ Assets/SpaceshipBoundaryWarning.cs
@@ -1,9 +1,12 @@
+using LiveLarson.SoundSystem;
 using UnityEngine;
 
 public class SpaceshipBoundaryWarning : MonoBehaviour
 {
     [SerializeField] private Collider headCollider;
     [SerializeField] private GameObject warningUI;
+    [SerializeField] private string warningSFX = "Assets/Audio/Warning.mp3";
+    private Audio _sfx;
 
     private void Awake()
     {
@@ -12,11 +15,19 @@ public class SpaceshipBoundaryWarning : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other == headCollider) warningUI.SetActive(true);
+        if (other == headCollider)
+        {
+            warningUI.SetActive(true);
+            _sfx = SoundService.PlaySfx(warningSFX, transform.position);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other == headCollider) warningUI.SetActive(false);
+        if (other == headCollider)
+        {
+            warningUI.SetActive(false);
+            _sfx?.Stop();
+        }
     }
 }
