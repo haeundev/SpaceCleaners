40514f5c64ab6b14558e49f9436f99e89528b2b6
haeundev <haeundev@gmail.com>
2023-09-19 23:06:04 +0900

    fixed ui script

--- Assets/ContestMapUIChange.cs
+++ Assets/ContestMapUIChange.cs
@@ -1,25 +1,19 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using DataTables;
-using LiveLarson.Enums;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ContestMapUIChange : MonoBehaviour
 {
     public GameObject contestMapImg;
-
     public Sprite[] mapImgs;
+    private Sprite _currentMapSprite;
 
-    private Sprite currentMapSprite;
-    // Start is called before the first frame update
     private void Awake()
     {
         TaskManager.Instance.OnTaskAcquired += OnTaskAcquired;
 
-        currentMapSprite = contestMapImg.GetComponent<Image>().sprite;
-        currentMapSprite = mapImgs[0];
+        var initialSprite = contestMapImg.GetComponent<Image>().sprite = mapImgs[0];
+        _currentMapSprite = initialSprite;
     }
 
     private void OnDestroy()
@@ -29,25 +23,15 @@ public class ContestMapUIChange : MonoBehaviour
 
     private void OnTaskAcquired(TaskInfo taskInfo)
     {
-        currentMapSprite = taskInfo.ID switch
+        _currentMapSprite = taskInfo.ID switch
         {
+            // 하드코딩
             4 => mapImgs[1],
             5 => mapImgs[2],
             8 => mapImgs[3],
             10 => mapImgs[4],
             13 => mapImgs[5],
-            _ => currentMapSprite
+            _ => _currentMapSprite
         };
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/LeaderboardUIChange.cs
+++ Assets/LeaderboardUIChange.cs
@@ -1,51 +1,35 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using DataTables;
-using LiveLarson.Enums;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LeaderboardUIChange : MonoBehaviour
 {
     public GameObject leaderboardImg;
-
     public Sprite[] leaderboardImgs;
-
-    private Sprite currentLeaderboardSprite;
-    // Start is called before the first frame update
+    private Sprite _currentLeaderboardSprite;
+    
     private void Awake()
     {
         TaskManager.Instance.OnTaskAcquired += OnTaskAcquired;
 
-        currentLeaderboardSprite = leaderboardImg.GetComponent<Image>().sprite;
-        currentLeaderboardSprite = leaderboardImgs[0];
+        var initialSprite = leaderboardImg.GetComponent<Image>().sprite = leaderboardImgs[0];
+        _currentLeaderboardSprite = initialSprite;
     }
-
+    
     private void OnDestroy()
     {
         TaskManager.Instance.OnTaskAcquired -= OnTaskAcquired;
     }
-
+    
     private void OnTaskAcquired(TaskInfo taskInfo)
     {
-        currentLeaderboardSprite = taskInfo.ID switch
+        leaderboardImg.GetComponent<Image>().sprite = taskInfo.ID switch
         {
+            // 하드코딩
             5 => leaderboardImgs[1],
             12 => leaderboardImgs[2],
             14 => leaderboardImgs[3],
-            _ => currentLeaderboardSprite
+            _ => _currentLeaderboardSprite
         };
-    }
-
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
